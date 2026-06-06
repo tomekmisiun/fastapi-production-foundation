e343fb1b64cd7c110d859a3cc04f74743c4900b3
Tomasz Misiun <tomek.misiun@gmail.com>
2026-06-06 20:47:02 +0200

    add users CRUD endpoints

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ from app.api.routes.admin import router as admin_router
 from app.api.routes.auth import router as auth_router
 from app.api.routes.health import router as health_router
 from app.core.config import settings
-
+from app.api.routes import users
 
 app = FastAPI(title=settings.app_name)
 
 app.include_router(health_router)
 app.include_router(auth_router)
 app.include_router(admin_router)
+app.include_router(users.router)
